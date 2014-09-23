336f76c5655663751746d31360d6ab4302fab54d
Katharine Chen <katharinechen.ny@gmail.com>
2014-09-23 15:04:16 -0700

    Fix README.md

---
+++ README.md
@@ -0,0 +1,16 @@
+#Prime Numbers 
+
+##About
+This project was completed by Katharine Chen and Scott Dudley for Epicodus (Week#3 Day #3). 
+
+##Description 
+When given a number, this method will returns all prime numbers less than that number using the Sieve of Eratosthenes.
+
+##Functionality 
+```ruby
+prime_sifting(5) ##return [2,3,5]
+prime_sifting(100) ##return [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]
+```
+
+##License
+MIT
--- spec/prime_sifting_spec.rb
+++ spec/prime_sifting_spec.rb
@@ -2,26 +2,20 @@ require('rspec')
 require('prime_sifting')
 
 describe("prime_sifting") do
-
   it("takes an argument and lists all prime numbers smaller than that number") do
     expect(prime_sifting(5)).to(eq([2,3,5]))
   end
-
   it("takes an argument and lists all prime numbers smaller than that number") do
     expect(prime_sifting(100)).to(eq([2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]))
   end
-
 end
 
 describe("nth_prime") do
-
   it("returns the nth prime number") do
     expect(nth_prime(4)).to(eq(7))
   end
-
   it("returns the nth prime number") do
     expect(nth_prime(20)).to(eq(71))
   end
-
 end
 
